9ed83bac13cb6d6331ec24de7e4883dea02442a6
Ajai Mani <amani@suyatitech.local>
2020-04-14 20:49:19 +0530

    semantics corrections

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -29,8 +29,7 @@ export class HomeComponent implements OnInit {
 
 }
 
-cancelRegisterMode(registerMode: boolean)
-{
+cancelRegisterMode(registerMode: boolean) {
   this.registerMode = registerMode;
 }
 
